53de273e72aefe93cf20bd9768a7cea2113607ae
Daniel Ahossou <danielahossou@gmail.com>
2025-05-27 13:53:13 -0700

    service workers initialized

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,15 +45,27 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if('serviceWorker' in navigator){
   // B2. TODO - Listen for the 'load' event on the window object.
+    window.addEventListener('load', () => {
+
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
   //            "Using Service Workers" will help you here)
+  navigator.serviceWorker.register('./sw.js').then((registration) => {
+
   // B4. TODO - Once the service worker has been successfully registered, console
   //            log that it was successful.
+  console.log('Service Worker has registered.')
+}).catch((err) => {
+
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
+  console.error('Service Worker has failed', err);
+});
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+    })
+  }
 }
 
 /**
@@ -90,7 +102,7 @@ async function getRecipes() {
   /**************************/
   // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
   //            declared above
-  for (const urls of  RECIPE_URLS){
+  for (const urls of RECIPE_URLS){
   // A5. TODO - Since we are going to be dealing with asynchronous code, create
   //            a try / catch block. A6-A9 will be in the try portion, A10-A11
   //            will be in the catch portion.
